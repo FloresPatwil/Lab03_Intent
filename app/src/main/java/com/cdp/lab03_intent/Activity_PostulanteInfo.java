4a0e3998b17075ca7b28dec1b93d6a024ddafdc6
FloresPatwil <pfloresri@unsa.edu.pe>
2023-10-20 13:49:17 -0500

    Diseno de las 4 activities

---
+++ app/src/main/java/com/cdp/lab03_intent/Activity_PostulanteInfo.java
@@ -0,0 +1,14 @@
+package com.cdp.lab03_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_PostulanteInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postulante_info);
+    }
+}
