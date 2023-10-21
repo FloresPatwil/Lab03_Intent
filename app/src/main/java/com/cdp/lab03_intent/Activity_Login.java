5575b87baf5734adaea66ab4cc28204e366f0ae8
FloresPatwil <pfloresri@unsa.edu.pe>
2023-10-20 22:34:37 -0500

    Codigo para Activity_Login, activity_menu.xml y modificacion de manifest

--- app/src/main/java/com/cdp/lab03_intent/Activity_Login.java
+++ app/src/main/java/com/cdp/lab03_intent/Activity_Login.java
@@ -2,7 +2,13 @@ package com.cdp.lab03_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Activity_Login extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class Activity_Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText edtUsuario = findViewById(R.id.edtUsuario);
+        EditText edtPassword = findViewById(R.id.edtPassword);
+        TextView txtMensaje = findViewById(R.id.txtMensaje);
+        Button btnLogin = (Button)findViewById(R.id.button);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario = edtUsuario.getText().toString();
+                String password = edtPassword.getText().toString();
+                if(usuario.equals("1234") && password.equals("1234")){
+                    Toast.makeText(getApplicationContext(), "Autenticacion valida", Toast.LENGTH_SHORT).show();
+                    txtMensaje.setText("Autenticacion valida");
+                    startActivity(new Intent(Activity_Login.this, Activity_Menu.class));
+                }else{
+                    txtMensaje.setText("Autenticacion invalida");
+                    Toast.makeText(getApplicationContext(), "Autenticacion no valida", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
